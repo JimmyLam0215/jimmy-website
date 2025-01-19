4f2c96faa03eff387ebb661928f89f277c44ed3d
JimmyLam0215 <lamlam200322@gmail.com>
2025-01-19 11:05:09 +0800

    Update SEO content

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,14 @@
 import profilePic from './assets/Jimmy.jpg'
 function Card(){
     return (
+        <main>
         <div className='card'>
             <img className='card-image' alt='profile picture' src={profilePic}></img>
-            <h2 className='card-title'>Jimmy Lam</h2>
+            <h1 className='card-title'>Lam Pui Wa, Jimmy</h1>
             <p>I am the normal one</p>
             <p className='copyright'>&copy; {new Date().getFullYear()} Jimmy Lam</p>
         </div> 
+        </main>
     );
 }
 
