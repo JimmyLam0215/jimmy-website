f947778c2dfd3569c060b61a474320886705d1a2
JimmyLam0215 <lamlam200322@gmail.com>
2025-06-30 18:30:49 +0800

    Update job experiences

--- src/JobExperiences.jsx
+++ src/JobExperiences.jsx
@@ -10,15 +10,15 @@ function JobExperiences () {
             <h3>Part-time Experiences:</h3>
             <tr>
                 <td className="job"><a href="https://candyservice.net/" target="_blank">💼 Candy Service Limited - Software Engineer</a></td>
-                <td className="job">[Sep 2024 – Now]</td>
+                <td className="job">[Sep 2024 – May 2025]</td>
             </tr>
             <tr>
                 <td className="job"><a href="https://www.polyu.edu.hk/" target="_blank">💼 The Hong Kong Polytechnic University - Student Assistant</a></td>
-                <td className="job">[Feb 2022 – April 2022]</td>
+                <td className="job">[Feb 2022 – Apr 2022]</td>
             </tr>
             <tr>
                 <td className="job"><a href="https://www.canapesroom.com/" target="_blank">💼 Canapés Room - Catering Helper</a></td>
-                <td className="job">[Sep 2021 – Now]</td>
+                <td className="job">[Sep 2021 – Jun 2025]</td>
             </tr>
         </table>
         <div className="divider"></div>
